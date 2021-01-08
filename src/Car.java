4af505d097ea2935eda2f1599aa9c29a11cd0083
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2021-01-08 03:55:09 -0500

    Finish setting up starting point

---
+++ src/Car.java
@@ -0,0 +1,53 @@
+import java.util.List;
+
+public class Car {
+
+    private final int gasLevel;
+    private final String color;
+
+    private final List<String> passengers;
+    private final List<String> trunkContents;
+
+    private Car(int gasLevel, String color, List<String> passengers, List<String> trunkContents) {
+        this.gasLevel = gasLevel;
+        this.color = color;
+        this.passengers = passengers;
+        this.trunkContents = trunkContents;
+    }
+
+    public static Car withGasColorPassengers(int gas, String color, String... passengers) {
+        List<String> p = List.of(passengers);
+        return new Car(gas, color, p, null);
+    }
+
+    public static Car withGasColorPassengersWithTrunkContent(int gas, String color, String... passengers) {
+        List<String> p = List.of(passengers);
+        return new Car(gas, color, List.of(passengers), List.of("jack", "wrench", "spare wheel"));
+    }
+
+    public int getGasLevel() {
+        return gasLevel;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<String> getPassengers() {
+        return passengers;
+    }
+
+    public List<String> getTrunkContents() {
+        return trunkContents;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "gasLevel=" + gasLevel +
+                ", color='" + color + '\'' +
+                ", passengers=" + passengers +
+                "," + (trunkContents == null ? "no trunk" : "trunkContents=" + trunkContents) +
+                '}';
+    }
+}
---
+++ src/CarScratch.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+public class CarScratch {
+
+
+
+    public static void showAll(List<Car> lc) {
+        for (Car c : lc) {
+            System.out.println(c);
+        }
+        System.out.println("-------------------------------------");
+    }
+
+
+
+
+    public static void main(String[] args) {
+        List<Car> cars = List.of(
+                Car.withGasColorPassengers(6, "Red", "Fred", "Jim", "Sheila"),
+                Car.withGasColorPassengers(3, "Octarine", "Rincewind", "Ridcully"),
+                Car.withGasColorPassengers(9, "Black", "Weatherwax", "Magrat"),
+                Car.withGasColorPassengers(7, "Green", "Valentine", "Gillian", "Anne", "Dr. Mahmoud"),
+                Car.withGasColorPassengers(6, "Red", "Ender", "Hyrum", "Locke", "Bonzo")
+        );
+        showAll(cars);
+    }
+}
