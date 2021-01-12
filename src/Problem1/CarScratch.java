15d248075ec18a081e0ab1f11a0cf8c9a51aa530
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2021-01-12 09:16:50 -0500

    Coding adapter in func

--- src/Problem1/CarScratch.java
+++ src/Problem1/CarScratch.java
@@ -1,12 +1,22 @@
 package Problem1;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 public class CarScratch {
+
+    public static <E> ToIntFunction<E> compareWithThis(E target, Comparator<E> comp) {
+        return c -> comp.compare(target, c);
+    }
+
+    public static <E> Predicate<E> isGreater(ToIntFunction<E> target){
+        return e -> target.applyAsInt(e) < 0;
+    }
 
     public static <E> void showAll(List<E> lc) {
         for (E c : lc) {
@@ -38,22 +48,28 @@ public class CarScratch {
 
 //        showAll(getByCriterion(cars, Car.getColorCriterion("Red", "Black")));
 
-        Predicate<Car> level7 = Car.getGasLevelCriterion(7);
-        showAll(getByCriterion(cars, level7));
+//        Predicate<Car> level7 = Car.getGasLevelCriterion(7);
+//        showAll(getByCriterion(cars, level7));
+//
+//        Predicate<Car> notLevel7 = Predicate.not(level7);
+//        showAll(getByCriterion(cars, notLevel7));
+//
+//
+//        Predicate<Car> isRed = Car.getColorCriterion("Red");
+//        Predicate<Car> fourPassengers = c -> c.getPassengers().size() == 4;
+//        Predicate<Car> redFourPassengers = isRed.and(fourPassengers);
+//        showAll(getByCriterion(cars, redFourPassengers));
+//
+//        Predicate<Car> isBlack = Car.getColorCriterion("Black");
+//        Predicate<Car> blackOrFourPassenger = isBlack.or(fourPassengers);
+//        showAll(getByCriterion(cars, blackOrFourPassenger));
 
-        Predicate<Car> notLevel7 = Predicate.not(level7);
-        showAll(getByCriterion(cars, notLevel7));
+        Car bert = Car.withGasColorPassengers(5, "Blue","Cameron","Eric","David");
 
+        ToIntFunction<Car> compareWithBert = compareWithThis(bert,Car.getPassengerCountOrder());
+        cars.forEach(c -> System.out.printf("Comparing %s with bert gives %d\n", c, compareWithBert.applyAsInt(c)));
 
-        Predicate<Car> isRed = Car.getColorCriterion("Red");
-        Predicate<Car> fourPassengers = c -> c.getPassengers().size() == 4;
-        Predicate<Car> redFourPassengers = isRed.and(fourPassengers);
-        showAll(getByCriterion(cars, redFourPassengers));
-
-        Predicate<Car> isBlack = Car.getColorCriterion("Black");
-        Predicate<Car> blackOrFourPassenger = isBlack.or(fourPassengers);
-        showAll(getByCriterion(cars, blackOrFourPassenger));
-
+        showAll(getByCriterion(cars,isGreater(compareWithBert)));
 
     }
 }
